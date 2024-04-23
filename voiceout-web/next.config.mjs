098962b82a466214b3625140b3cd405564bcaff5
NoahMarkDelaPena <noahmarkdelapena@gmail.com>
2024-04-23 12:32:28 +0800

    setup: PWA / Jest / packages

---
+++ voiceout-web/next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+const nextConfig = { reactStrictMode: true };
+export default withPWA({
+  nextConfig,
+});
