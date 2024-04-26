09406ec7daf44949fe2a04536be91f684a7d0782
Carl James Roxas <108602920+Just-a-NoobieDev@users.noreply.github.com>
2024-04-26 17:40:21 +0800

    fix: type error in prod build

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "jest";
+// import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
-const config: Config = {
+const config = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   coverageProvider: "v8",
   testEnvironment: "jsdom",
